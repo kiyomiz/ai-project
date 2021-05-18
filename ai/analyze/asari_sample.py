aa85b08bdb410b33d51d937e0d24ba403dc3e544
kiyoshimizutani <k_mizutani@aseit.jp>
2021-05-19 00:57:26 +0900

    感情分析 asari

---
+++ ai/analyze/asari_sample.py
@@ -0,0 +1,21 @@
+from asari.api import Sonar
+import sklearn
+
+print(sklearn.__version__)
+
+list_text = [
+    'この人は、この世の中で、いちばんしあわせな人にちがいありません。',
+    '芝居小屋もすばらしいし、お客さんもすばらしい人たちでした。',
+    'もし中世の時代だったら、おそらく、火あぶりにされたでしょうよ。',
+    'みんなのうるさいことといったら、まるで、ハエがびんの中で、ブンブンいっているようでした。',
+    'われわれ人間が、こういうことを考えだすことができるとすれば、われわれは、地の中にうめられるままに'
+]
+
+sonar = Sonar()
+res = sonar.ping(text="広告多すぎる")
+print(res)
+res = sonar.ping(text="サイコー")
+print(res)
+
+res = list(map(sonar.ping, list_text))
+print(res)
